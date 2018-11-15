edf35e7462a8ddee41e2b2dec408a43a32ec2ed5
Chin Wang <chinxw@gmail.com>
2018-11-15 16:16:42 +1100

    initializing one time jwt

--- aviation/views.py
+++ aviation/views.py
@@ -4,9 +4,10 @@ title_text = "AviationVR"
 template_name = 'aviation/index.html'
 
 def index(request):
-    if 'ext_user_username' in request.session:
+    if 'zid' in request.session:
         context = {
-            'title_text' : title_text,
+            'title_text' : request.session['hello'],
+            # 'title_text' : title_text,
             'manual_literoomjs' : 'js/LITEroomJS/LITEroom_0.37Beta.js',
         }
         return render(request, template_name, context)
